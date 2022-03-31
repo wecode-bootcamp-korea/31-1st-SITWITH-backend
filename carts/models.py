a3e2b3ecf5314f47b2a6418776164cce23bbea4d
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-03-31 15:36:00 +0900

    [Modify]models

--- carts/models.py
+++ carts/models.py
@@ -3,9 +3,9 @@ from django.db    import models
 from cores.models import TimestampZone
 
 class Cart(TimestampZone): 
-    user     = models.ForeignKey('users.user', on_delete=models.CASCADE)
-    product  = models.ForeignKey('products.product', on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    user         = models.ForeignKey('users.user', on_delete=models.CASCADE)
+    productcolor = models.ForeignKey('products.productcolor', on_delete=models.CASCADE)
+    quantity     = models.IntegerField()
 
     def total_price(self): 
         return self.quantity * self.product.price
--- sitwith/settings.py
+++ sitwith/settings.py
@@ -16,6 +16,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+ALGORITHM = ALGORITHM
 
 # Application definition
 
