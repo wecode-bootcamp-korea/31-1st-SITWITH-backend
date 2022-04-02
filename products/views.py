32053e094d016e0ea93b96fb1d3a432b4a44b629
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-04-02 17:24:08 +0900

    [Modify]views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+
 from django.http            import JsonResponse
 from django.views           import View
 
@@ -7,29 +8,29 @@ from .models import Product, Color, Image, ProductColor
 class ProductListView(View): 
     def get(self, request): 
         category = request.GET.get('category', "all")
-        
+        res = []
         if category != 'all':
            product_data = Product.objects.filter(category_id=category)
-
         else: 
             product_data = Product.objects.all()
 
         if not product_data: 
             return JsonResponse({"message":"Category does not exist"}, status = 400)
-
+     
         for product in product_data: 
             productcolor_data = ProductColor.objects.filter(product_id = product.id)
-            res=[{
-                "name" : product.name,
-                "price": product.price,
-                "color": productcolor.color.name,
-                "main_image": [image.image_url for image in Image.objects.filter(product_color_id = productcolor.id)][0]
-            }for productcolor in productcolor_data]
-
-            result=[{
+            result = []
+            for productcolor in productcolor_data: 
+                res.append({
+                    "name" : product.name,
+                    "price": product.price,
+                    "color": productcolor.color.name,
+                    "main_image": [image.image_url for image in Image.objects.filter(product_color_id = productcolor.id)][0]
+                })
+            result.append({
                 "category_id" : category,
                 "product_info": res
-            }]
+            })
         
         return JsonResponse({"message":"Success", "result":result}, status = 200)
     
@@ -51,7 +52,6 @@ class DetailView(View):
 
             if not color_data:
                 return JsonResponse({"message":"Color does not exist"}, status = 400)
-            
             result = [{
                 "name"      : data.product.name,
                 "price"     : product.price,
