a70213af063298c8c6fba14c99a8a13e62a0fc1c
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-04-05 14:32:45 +0900

    [Modify]Add Search API

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DetailView,ProductListView
+from .views import DetailView,ProductListView,SmartSearchView
 
 urlpatterns = [
     path('',ProductListView.as_view()),
-    path('/<int:product_id>', DetailView.as_view())
+    path('/<int:product_id>', DetailView.as_view()),
+    path('/smart',SmartSearchView.as_view())
 ]
