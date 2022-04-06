b350806f76f89628c5a55ca67b33246980044c78
angrybirdpark <thesmileless90@gmail.com>
2022-04-06 17:10:58 +0900

    Modify : 라이브 코드 피드백 적용

--- carts/validator.py
+++ carts/validator.py
@@ -1,9 +1,5 @@
 from django.forms import ValidationError
-
+    
 def validate_quantity(quantity):
     if quantity <= 0:
         raise ValidationError('Quantity must be positive number')
-    
-def validate_total_quantity(quantity, inventory):
-    if quantity > inventory:
-        raise ValidationError('Quantity cannot be more than inventory')
--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,11 @@
 import json
 
-from django.http  import JsonResponse
+from django.http  import JsonResponse, HttpResponse
 from django.views import View
 from django.forms import ValidationError
 
 from carts.models    import Cart
-from carts.validator import validate_quantity, validate_total_quantity
+from carts.validator import validate_quantity
 from cores.decorator import login_authorization
 from products.models import ProductColor, Image
 
@@ -15,11 +15,9 @@ class CartView(View):
         try:
             data          = json.loads(request.body)
             user          = request.user
-            product_id    = data['product_id']
-            color_id      = data['color_id']
-            quantity      = data['quantity']
-            product_color = ProductColor.objects.get(product_id = product_id, color_id = color_id)
-            inventory     = product_color.inventory
+            productcolor_id = data['productcolor_id']
+            quantity        = data['quantity']
+            product_color   = ProductColor.objects.get(id = productcolor_id)
             
             validate_quantity(quantity)
             
@@ -29,15 +27,9 @@ class CartView(View):
                 defaults      = {'quantity' : 0}
             )
             cart.quantity += quantity
-            
-            validate_total_quantity(cart.quantity, inventory)
-            
             cart.save()
             
-            if is_created:
-                return JsonResponse({'message' : 'Cart created'}, status=200)
-            else:
-                return JsonResponse({'message' : 'Cart updated'}, status=200)
+            return HttpResponse(status=200)
         except ProductColor.DoesNotExist:
             return JsonResponse({'message' : 'Product does not exist'}, status=400)
         except KeyError:
@@ -51,19 +43,18 @@ class CartView(View):
         user      = request.user
         cart_data = Cart.objects.filter(user = user)            
         
-        if not cart_data.exists():
-            return JsonResponse({'result' : []}, status=404)
-
         result = [{
             'cart_id'      : cart.id,
+            'product_id'   : cart.product_color_id,
             'category_name': cart.product_color.product.category.name,
             'quantity'     : cart.quantity,
             'product_name' : cart.product_color.product.name,
             'product_price': cart.product_color.product.price,
             'color_name'   : cart.product_color.color.name,
             'image_url'    : Image.objects.get(
-                                product_color_id = cart.product_color.id,
-                                sequence         = 1).image_url
+                                sequence         = 1,
+                                product_color_id = cart.product_color.id
+                             ).image_url
         } for cart in cart_data]
         
         return JsonResponse({'result' : result}, status=200)
@@ -75,17 +66,10 @@ class CartView(View):
             user      = request.user
             quantity  = data['quantity']
             cart      = Cart.objects.get(user = user, id = int(cart_id))
-            inventory = cart.product_color.inventory
-
-            if not cart_id:
-                return JsonResponse({'message' : 'Url path is wrong'}, status=400)
 
             validate_quantity(quantity)
             
             cart.quantity = quantity
-            
-            validate_total_quantity(cart.quantity, inventory)
-            
             cart.save()
 
             return JsonResponse({'quantity': cart.quantity}, status=200)
@@ -105,10 +89,6 @@ class CartView(View):
             return JsonResponse({'message' : 'Cart does not exist'}, status=400)
         
         cart = Cart.objects.filter(user = user, id__in = cart_id_list)
-        
-        if not cart:
-            return JsonResponse({'message' : 'Cart does not exist'}, status=400)
-        
         cart.delete()
         
-        return JsonResponse({'message' : 'Cart deleted'}, status=200)
+        return HttpResponse(status=204)
